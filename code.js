7adef1cb04213119f286d7d936f18a786421c5dd
--replace-all <--replace-all>
2023-11-23 12:22:58 +0530

    add navigate to cgpa or sgpa calc page

---
+++ code.js
@@ -0,0 +1,15 @@
+function submitmain() {
+    let check, i;
+    for (i = 0; i < 2; i++) {
+        let a = document.getElementsByName("sc");
+        if (a[i].checked) {
+            check = a[i].value;
+            break;
+        }
+    }
+    if (check === "cgpa") {
+        window.open("cgpacode.html");
+    } else {
+        window.open("sgpacode.html");
+    }
+}
