f5fb3916843c61c5c214fc2329fd44412c9e9389
--replace-all <--replace-all>
2023-11-23 12:52:21 +0530

    add basic strcuture

---
+++ sgpacode.js
@@ -0,0 +1,40 @@
+
+function sgpabtn(){
+ globalcredit=document.getElementById("totalcredit").value;
+ globalsubject=document.getElementById("noofsub").value;
+ window.open("dataentry.html");
+}
+function calc(){
+    count=0;
+    let cgpa=0;
+    while(count<globalsubject){
+         cgpa+=grade(document.getElementById("marks").value) * document.getElementById("credit").value
+    }
+    cgpa/=(count+1);
+    open("code.html");
+    document,getElementById("codep").value="Your CGPA is "+cgpa;
+    
+}
+function grade(num){
+    if(num>=90){
+        return 10;
+    }
+    else if(num>=80&&num<90){
+        return 9;
+    }
+    else if(num>=70&&num<80){
+        return 8;
+    }
+    else if(num>=60&&num<70){
+        return 7;
+    }
+    else if(num>=55&&num<60){
+        return 6;
+    }
+    else if(num>=50&&num<55){
+        return 5;
+    }
+    else{
+        return 0;
+    }
+}
